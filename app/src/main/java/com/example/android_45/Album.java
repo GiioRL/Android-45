5b1d9a006fc53bb15748cb30a996ceced1f7d643
Rohit Bhagat <132160385+rb0125@users.noreply.github.com>
2025-12-06 20:22:29 -0500

    Photo Album Tag

---
+++ app/src/main/java/com/example/android_45/Album.java
@@ -0,0 +1,105 @@
+package com.example.android_45;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Represents a photo album belonging to a specific User.
+ * <p>
+ * An {@code Album} stores a list of {@link Photo} objects as well as metadata
+ * such as the album name and the owning user. Although this is primarily a
+ * model class, it also maintains several transient JavaFX components used
+ * for rendering album views and thumbnails.
+ * </p>
+ *
+ * <p><b>Note:</b> Transient fields (FXML nodes, controllers, and scenes)
+ * are recreated when needed, since they cannot be serialized. The underlying
+ * album data ({@code name}, {@code photos}, {@code user}) is fully
+ * serializable.</p>
+ */
+public class Album implements Serializable {
+
+    /** The name of the album. */
+    private String name;
+
+    /** The list of photos contained in this album. */
+    private ArrayList<Photo> photos;
+
+    /**
+     * Creates an empty album with the specified name and owner.
+     *
+     * @param name the album name
+     */
+    public Album(String name) {
+        this.name = name;
+        this.photos = new ArrayList<>();
+    }
+
+    /**
+     * Creates an album with a predefined list of photos.
+     *
+     * @param photos the photos to include
+     * @param name   the album name
+     */
+    public Album(ArrayList<Photo> photos, String name) {
+        this.photos = photos;
+        this.name = name;
+    }
+
+    /**
+     * Returns the list of photos in this album.
+     *
+     * @return the list of {@link Photo} objects
+     */
+    public ArrayList<Photo> getPhotos() {
+        return photos;
+    }
+
+    /**
+     * Returns the album name.
+     *
+     * @return the name of the album
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets a new name for the album.
+     *
+     * @param newName the updated name
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    /**
+     * Returns to the user’s album list view.
+     * <p>
+     * The thumbnail is recreated to refresh its graphics.
+     * </p>
+     */
+    public void back() {
+
+    }
+
+    /**
+     * Removes this album from its owner's collection.
+     */
+    public void delete() {
+
+    }
+
+    /**
+     * Creates a shallow clone of this album.
+     * <p>
+     * The clone references the same {@code photos} list and user,
+     * but is a distinct Album instance.
+     * </p>
+     *
+     * @return a cloned {@code Album} instance
+     */
+    public Album clone() {
+        return new Album(photos, name);
+    }
+}
---
+++ app/src/main/java/com/example/android_45/Photo.java
@@ -0,0 +1,83 @@
+package com.example.android_45;
+
+import android.net.Uri;
+
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a photo stored in an album.
+ * <p>
+ * A {@code Photo} object maintains information about its file location,
+ * caption, tags, and date. Because this is a serializable model class that
+ * also interacts with JavaFX, certain UI fields (e.g., thumbnail, controllers)
+ * are declared {@code transient} and recreated during deserialization.
+ * </p>
+ */
+public class Photo implements Serializable {
+
+    /** The file path of the image. */
+    private Uri uri;
+
+    /** The list of tags assigned to this photo. */
+    private ArrayList<Tag> tags;
+
+    /**
+     * Constructs a new photo.
+     * @param uri the file path to the photo
+     * @param tags     the list of tags assigned to the photo
+     */
+    public Photo(Uri uri, ArrayList<Tag> tags) {
+        this.uri = uri;
+        this.tags = tags;
+    }
+
+    /**
+     * Returns the list of tags assigned to the photo.
+     *
+     * @return the list of {@link Tag} objects
+     */
+    public ArrayList<Tag> getTags() {
+        return tags;
+    }
+
+    /**
+     * Replaces the existing tag list with a new set of tags.
+     *
+     * @param newTags the updated tag list
+     */
+    public void setTags(ArrayList<Tag> newTags) {
+        tags = newTags;
+    }
+
+    /**
+     * Returns the file path of the photo.
+     *
+     * @return the photo's location
+     */
+    public Uri getUri() {
+        return uri;
+    }
+
+    /**
+     * Custom deserialization logic.
+     * <p>
+     * After the photo's base data is deserialized, transient UI fields such
+     * as {@code image} and the thumbnail must be recreated manually.
+     * </p>
+     *
+     * @param in the input stream
+     * @throws IOException if reading fails
+     * @throws ClassNotFoundException if class lookup fails
+     */
+    private void readObject(ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
+
+        in.defaultReadObject();
+//        image = createImage();
+//        createThumbnail();
+    }
+}
